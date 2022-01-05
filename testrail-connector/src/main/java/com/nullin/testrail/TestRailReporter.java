6c7dae11b2d9626268b9857accef473fc7ed8e8e
Nilesh Vikas Yeole <nileshye@cybage.com>
2022-01-05 09:46:00 +0530

    updated logger line

--- testrail-connector/src/main/java/com/nullin/testrail/TestRailReporter.java
+++ testrail-connector/src/main/java/com/nullin/testrail/TestRailReporter.java
@@ -276,7 +276,7 @@ public class TestRailReporter {
 					client.addResultForCase(runId, caseId, body);
 
 				} catch (Exception ex) {
-					logger.severe("Test case "+case_id+ " is not pat of "+runId+" run id. Therefore status is not updated.");
+					logger.severe("Test case "+case_id+ " is not pat of "+runId+" run id. Therefore status is not updated." + ex);
 
 				}
 
